a3729ea47fda451990498b2c9ab70ac961b776a4
Jan Marthedal Rasmussen <jan@janmr.com>
2020-05-01 13:25:34 +0200

    Clear branch

--- src/client.js
+++
@@ -1,7 +0,0 @@
-import make_element from './trominobox';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = make_element(React, ReactDOM);
-
-ReactDOM.render(element, document.getElementById('tromino-fig'));
--- src/server.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
-import make_element from './trominobox';
-
-let element = make_element(React, ReactDOM);
-
-console.log(ReactDOMServer.renderToStaticMarkup(element));
